bd325cc0e96a6a9eabcbe2cc4ea39d8e66f3efc7
chethanck <chethanckm66@gmail.com>
2022-01-14 11:05:35 +0530

    Post changes

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
-const express = require("express");
-const { randomBytes } = require("crypto");
+const express = require('express');
+const { randomBytes } = require('crypto');
 const cors = require('cors');
-const axios = require("axios");
+const axios = require('axios');
 
 const app = express();
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 app.use(cors());
 
 const posts = {};
 
 app.get('/posts', (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
 app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
-    posts[id] = { id, title };
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  posts[id] = { id, title };
 
-    await axios.post("http://localhost:4005/events", {
-        type: 'PostCreated',
-        data: {
-            id, title
-        }
-    });
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title,
+    },
+  });
 
-    res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
 });
 
-app.post("/events", (req, res) => {
-    console.log("Received event", req.body.type);
+app.post('/events', (req, res) => {
+  console.log('Received event', req.body.type);
 
-    res.send({});
+  res.send({});
 });
 
 app.listen(4000, () => {
-    console.log("Post server starting on port 4000");
+  console.log('Changing version 600');
+  console.log('Post server starting on port 4000');
 });
